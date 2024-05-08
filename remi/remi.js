9d2eaec2dddcc9ddd2c0afdb6053ab76ddbc0c72
John Young <theroad2eldorado@gmail.com>
2024-05-08 07:57:01 -0400

    🐛 Delays swapping so js altered images have more time to populate srcs

--- remi/remi.js
+++ remi/remi.js
@@ -1,28 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const images = document.querySelectorAll('img');
-  for (let i = 0; i < images.length; i++) {
-    const img = images[i];
-    const src = img.getAttribute('src');
-    fetch(ajax_object.ajax_url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        action: 'remi_remote_image',
-        src,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.swapped) {
-          img.setAttribute('src', data.swapped);
-          console.log('Remi Swapped: ', data.swapped);
-        }
-        if (data.downloaded) {
-          console.log('Remi Downloaded: ', data.downloaded);
-        }
+  setTimeout(() => {
+    const images = document.querySelectorAll('img');
+    for (let i = 0; i < images.length; i++) {
+      const img = images[i];
+      const src = img.getAttribute('src');
+      fetch(ajax_object.ajax_url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          action: 'remi_remote_image',
+          src,
+        }),
       })
-      .catch((error) => console.error('Error:', error));
-  }
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.swapped) {
+            img.setAttribute('src', data.swapped);
+            console.log('Remi Swapped: ', data.swapped);
+          }
+          if (data.downloaded) {
+            console.log('Remi Downloaded: ', data.downloaded);
+          }
+        })
+        .catch((error) => console.error('Error:', error));
+    }
+  }, 300);
 });
